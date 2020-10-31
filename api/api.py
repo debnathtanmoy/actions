65af20d56cc68f4712861e423fcbfe992f0b43b0
Tanmoy Debnath <Tanmoy.Debnath@philips.com>
2020-10-31 22:27:11 +0530

    reduced limit complexity

--- api/api.py
+++ api/api.py
@@ -24,10 +24,17 @@ def get_prod():
 
 @app.route('/getprodid/<int:task_id>', methods=['GET'])
 def get_prod_by_id(task_id):
-    task = [task for task in products if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+    # task = [task for task in products if task['id'] == task_id]
+    # if len(task) == 0:
+    #     abort(404)
+    # return jsonify({'task': task[0]})
+    for task in products:
+        if task['id'] == task_id:
+            # if len(task) == 0:
+            #     abort(404)
+            tasks = [task]
+            return jsonify({'task': tasks[0]}),200
+            
 
 
 @app.route('/addprod',methods=['POST'])
@@ -42,11 +49,19 @@ def addproduct():
 
 @app.route('/deleteprod/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    task = [task for task in products if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    products.remove(task[0])
-    return jsonify({'result': "Deleted"}),200
+    # task = [task for task in products if task['id'] == task_id]
+    # if len(task) == 0:
+    #     abort(404)
+    # products.remove(task[0])
+    # return jsonify({'result': "Deleted"}),200
+    for task in products:
+        if task['id'] == task_id:
+            # if len(task) == 0:
+            #     abort(404)
+            tasks = [task]
+            products.remove(tasks[0])
+            return jsonify({'result': "Deleted"}),200
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
